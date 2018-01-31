493c5014c5762533c31ca181d1a74f36d1ff341d
cannyco <cannycophp@gmail.com>
2018-01-31 09:42:33 +0800

    Update init_system.sh

--- for-php/docker-php-mysql-nginx/init_system.sh
+++ for-php/docker-php-mysql-nginx/init_system.sh
@@ -23,7 +23,8 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 #然后是docker-composer
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 #然后是建docker的php开发环境
 mkdir /Dockerfiles
